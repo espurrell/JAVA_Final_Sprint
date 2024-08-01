b133f8d03343a2daf39ac777b3a061a771606ee4
Charity Smith <150025121+csmith709@users.noreply.github.com>
2024-08-01 12:38:13 -0230

    not actually fixed

--- src/main/java/database/DatabaseConnection.java
+++ src/main/java/database/DatabaseConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
 
     private static final String URL = "jdbc:postgresql://localhost:5432/vintique";
-    private static final String USER = "postres";
+    private static final String USER = "postgres";
     private static final String PASSWORD = "Keyin2021";
 
     public static void main(String[] args) {
